651650e2d1fb57d6dc47c3e13f07e933d393dcf7
praveenkrao94 <praveenkr0812@gmail.com>
2023-06-22 00:01:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+// app.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+
+            },
+            768: {
+                slidesPerView: 2,
+
+            },
+            1024: {
+                slidesPerView: 2,
+
+            },
+        },
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper1", {
+        slidesPerView: 3,
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+
+            },
+            768: {
+                slidesPerView: 2,
+
+            },
+            1024: {
+                slidesPerView: 4,
+
+
+
+            },
+        },
+    });
+});
